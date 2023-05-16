35dcd71eca57b2303e9e756796ab6d2659a4312c
Stephen Vertucci <steve.vertucci@gmail.com>
2023-05-16 19:34:05 -0400

    created DB with 2 tables, attached to DB in server.js

--- database.sql
+++ database.sql
@@ -8,3 +8,26 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+-- This is the DB SQL 
+
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "user_name" VARCHAR (15) NOT NULL,
+    "password" VARCHAR (15) NOT NULL,
+    "daily_goal" INTEGER 
+);
+
+CREATE TABLE "entry" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER REFERENCES "users" (id),
+    "amount" INTEGER, 
+    "data_date" DATE 
+); 
+
+INSERT INTO "users" ("user_name", "password", "daily_goal")
+VALUES ('');
+
+INSERT INTO "entry"  ("user_id", "amount", "data_date")
+VALUES ("$1", "$2", "$3");
